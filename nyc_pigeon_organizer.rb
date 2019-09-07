36167bc31a27ddc57dc7d8c2d6ed85be0e1636a3
Alfonso Socarras <asocarras10@gmail.com>
2019-09-07 15:04:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+
+def nyc_pigeon_organizer(pigeon_data)
+  
+  male_names = pigeon_data[:gender][:male]
+  female_names = pigeon_data[:gender][:female]
+  names = male_names << female_names
+  names = names.flatten
+  
+  colors = pigeon_data[:color]
+ 
+  
+  pigeon_list = Hash[names.collect { |name| [name, { :color => [], :gender => [], :lives => [] }] } ]
+  
+  
+    
+  #binding.pry
+
+ 
 end
+
+
+
+
+
+
+
+
+
+
+
+
